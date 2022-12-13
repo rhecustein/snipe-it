681c2e47c0d0ea35b13d8b9c9909c644567ed670
rhecustein <bintangwijaya18@gmail.com>
2022-12-13 14:21:57 +0000

    remove vendor_old

--- vendor_old/eduardokum/laravel-mail-auto-embed/src/Embedder/Embedder.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Eduardokum\LaravelMailAutoEmbed\Embedder;
-
-abstract class Embedder implements UrlEmbedder, EntityEmbedder
-{
-}
--- vendor_old/rollbar/rollbar/src/ScrubberInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Rollbar;
-
-interface ScrubberInterface
-{
-    public function scrub(&$data, $replacement);
-}
